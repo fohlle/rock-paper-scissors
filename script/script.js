18f8d47e036a8d25a34826d8622b13c70f2f506b
fohlle <patte2000@hotmail.com>
2020-07-15 22:52:18 +0200

    script to show whos won

--- script/script.js
+++ script/script.js
@@ -11,42 +11,76 @@ function computerChoice(){
     }
 }
 
-function playerChoice(text){
-    text = text.toLowerCase();
+/*
+function playerChoice(){
+    text = testText.toLowerCase();
     while(!(text == "rock" || text == "paper" || text == "scissors")){
         text = prompt("rock, paper, scissors").toLowerCase();
     }
     return text;
 }
+*/
 
 const cp1 = computerChoice();
-const player = playerChoice("scissors");
+// const player = playerChoice("");
 
 function round(cp1, player){
     console.log(cp1 + " " + player)
+    let text = "";
     if(cp1 == "rock" && player == "scissors"){
-        return "You lose " + cp1 + " beats " + player;
+        text = "You lose " + cp1 + " beats " + player;
+        printToPara(text);
+        return;
     }
     if(cp1 == "paper" && player == "rock"){
-        return "You lose " + cp1 + " beats " + player;
+        text = "You lose " + cp1 + " beats " + player;
+        printToPara(text);
+        return;
     }
     if(cp1 == "scissors" && player == "paper"){
-        return "You lose " + cp1 + " beats " + player
+        text = "You lose " + cp1 + " beats " + player
+        printToPara(text);
+        return;
     }
     if(cp1 == "rock" && player == "rock"){
-        return "Draw";
+        text = "Draw";
+        printToPara(text);
+        return;
     }
     if(cp1 == "paper" && player == "paper"){
-        return "Draw";
+        text = "Draw";
+        printToPara(text);
+        return;
     }
     if(cp1 == "scissors" && player == "scissors"){
-        return "Draw";
+        text = "Draw";
+        printToPara(text);
+        return;
     }
     else{
-        return "You win " + player + " beats " + cp1;
+        text = "You win " + player + " beats " + cp1;
+        printToPara(text);
+        return;
     }
-    
-
 }
+let testText = "";
+const rockBtn = document.querySelector("#rock");
+rockBtn.addEventListener("click", () =>{
+    testText = "rock";
+    console.log(round(computerChoice(), testText));
+});
+const paperBtn = document.querySelector("#paper");
+paperBtn.addEventListener("click", () =>{
+    testText = "paper";
+    console.log(round(computerChoice(), testText));
+});
+const scissorsBtn = document.querySelector("#scissors");
+scissorsBtn.addEventListener("click", () =>{
+    testText = "scissors";
+    console.log(round(computerChoice(), testText));
+});
 
-console.log(round(cp1, player));
+function printToPara(string){
+    let para = document.querySelector("#paraRes");
+    para.textContent = string;
+}
